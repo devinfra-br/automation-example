c122ff3bfe25700acbb96ac2a28fc44b23b715ac
Wilton Guilherme <wilton.guilherme@picpay.com.br>
2020-09-22 15:08:19 -0300

    Ajustes

--- iac/bash-scripts/ansible-app-deploy.sh
+++ iac/bash-scripts/ansible-app-deploy.sh
@@ -6,7 +6,7 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 tipo=$1
 host=$2
 
-ANSIBLE_HOST_KEY_CHECKING=False  ansible-playbook automation-example/iac/ansible/tasks/configure-$tipo.yml -i automation-example/iac/ansible/inventory/$host.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook automation-example/iac/ansible/tasks/configure-$tipo.yml -i automation-example/iac/ansible/inventory/$host.yml
 
 
 #ansible -i iac/ansible/inventory/server2.yml -m ping
