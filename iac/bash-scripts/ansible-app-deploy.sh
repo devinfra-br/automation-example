433e9f06e8c84172f0553023f71788d68ae47412
Wilton Guilherme <wilton.guilherme@picpay.com.br>
2020-09-22 18:06:57 -0300

    Update Ansible

--- iac/bash-scripts/ansible-app-deploy.sh
+++ iac/bash-scripts/ansible-app-deploy.sh
@@ -8,5 +8,3 @@ host=$2
 
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook automation-example/iac/ansible/tasks/configure-$tipo.yml -i automation-example/iac/ansible/inventory/$host.yml
 
-
-#ansible -i iac/ansible/inventory/server2.yml -m ping
